a3a0c5f27a6a6e2b7f5ece49452fabc0c27ec656
Mat Schaffer <mat@schaffer.me>
2011-05-05 09:26:56 -0400

    Order events by starts at

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,7 +2,7 @@ class Admin::EventsController < AdministrativeController
   # GET /events
   # GET /events.xml
   def index
-    @events = Event.find(:all)
+    @events = Event.find(:all).order(:starts_at)
 
     respond_to do |format|
       format.html # index.html.erb
